ef403e464951da38fc30e2546fd4f2339ca9c1c9
Tchikawa <k10d2019@gmail.com>
2019-12-12 01:36:08 +0900

    delete line

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,9 @@
-import { GET_OSAKE_LIST, GET_OSAKE_DETAIL } from '../utils/actionTypes';
+import { GET_OSAKE_LIST, GET_OSAKE_DETAIL } from '../utils/actionTypes'
 
 export const getOsakeList = () => ({
   type: GET_OSAKE_LIST,
-});
-
+})
 
 export const getOsakeDetail = (osake) => ({
     type: GET_OSAKE_DETAIL, osake
 })
-
